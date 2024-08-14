f2fc6c27393ca5598786d197e741decf15beb995
rhii0001 <rhii0001@student.monash.edu>
2024-08-15 01:47:41 +1000

    Add new constants, Action interface, and types to define State and Circle

--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,16 @@
 export { Constants, Viewport, Note };
-export type { csvLine, Key, Event };
+export type { csvLine, Key, Event, State, Circle, Action };
 
 /** Constants */
 
 const Constants = {
-  TICK_RATE_MS: 500,
-  SONG_NAME: "RockinRobin",
+  TICK_RATE_MS: 10,
+  SONG_NAME: "SleepingBeauty",
   MAX_MIDI_VELOCITY: 127,
   NUMBER_OF_COLUMNS: 4,
+  COLUMN_WIDTH: 20,
+  NOTE_COLORS: ["green", "red", "blue", "yellow"],
+  COLUMN_KEYS: ["KeyH", "KeyJ", "KeyK", "KeyL"],
 } as const;
 
 const Viewport = {
@@ -28,6 +31,24 @@ type Event = "keydown" | "keyup" | "keypress";
 
 /** Types */
 
+type State = Readonly<{
+  score: number;
+  time: number;
+  circles: ReadonlyArray<Circle>;
+  exit: ReadonlyArray<Circle>;
+  gameEnd: boolean;
+}>;
+
+type Circle = Readonly<{
+  id: number;
+  x: number;
+  y: number;
+  column: number;
+  start: number;
+  clickedTime?: number;
+  note: csvLine;
+}>;
+
 type csvLine = Readonly<{
   user_played: boolean;
   instrument_name: string;
@@ -36,3 +57,10 @@ type csvLine = Readonly<{
   start: number;
   end: number;
 }>;
+
+/**
+ * Actions modify state
+ */
+interface Action {
+  apply(s: State): State;
+}
