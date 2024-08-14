3c32c76dff2ab51ccfb555072e0b52de7029219d
rhii0001 <rhii0001@student.monash.edu>
2024-08-15 01:47:03 +1000

    Add initialState, createCircle, Tick to handle the actions on each tick of the game including moving the circles, CreateCircle, and reduceState

--- src/state.ts
+++ src/state.ts
@@ -1,17 +1,93 @@
-/** State processing */
+export {
+  initialState,
+  Tick,
+  reduceState,
+  Placeholder,
+  CreateCircle,
+  createCircle,
+};
 
-type State = Readonly<{
-  gameEnd: boolean;
-}>;
+import { Action, State, Circle, csvLine } from "./types";
+import { not } from "./util";
+
+///////////////////
+// INITIAL SETUP //
+///////////////////
 
 const initialState: State = {
+  score: 0,
+  time: 0,
+  circles: [],
+  exit: [],
   gameEnd: false,
 } as const;
 
+const createCircle =
+  (id: number) =>
+  (column: number) =>
+  (start: number) =>
+  (note: csvLine) =>
+  (x: number) =>
+  (y: number): Circle => {
+    return {
+      id,
+      x,
+      y,
+      column,
+      start,
+      note,
+    };
+  };
+
+class Placeholder implements Action {
+  apply(s: State): State {
+    return s;
+  }
+}
+
+class Tick implements Action {
+  constructor(public readonly elapsed: number) {}
+
+  apply(s: State): State {
+    const expired = (circle: Circle) => {
+      return circle.y >= 350;
+    };
+    const expiredCircles = s.circles.filter(expired);
+    const activeCircles = s.circles.filter(not(expired));
+
+    return {
+      ...s,
+      time: this.elapsed,
+      circles: activeCircles.map(Tick.moveCircle),
+      exit: expiredCircles,
+    };
+  }
+
+  static moveCircle = (circle: Circle): Circle => {
+    return {
+      ...circle,
+      y: circle.y + 3.5,
+    };
+  };
+}
+
+class CreateCircle implements Action {
+  constructor(public readonly circle: Circle) {}
+
+  apply(s: State): State {
+    return {
+      ...s,
+      circles: [...s.circles, this.circle],
+    };
+  }
+}
+
+// class ClickCircle implements Action {}
+
 /**
- * Updates the state by proceeding with one time step.
- *
- * @param s Current state
- * @returns Updated state
+ * state transducer
+ * @param s input State
+ * @param action type of action to apply to the State
+ * @returns a new State
  */
-const tick = (s: State) => s;
+const reduceState = (s: State, action: Action): State => action.apply(s);
