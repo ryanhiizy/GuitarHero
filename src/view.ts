330935cd6abe76bcf6c1124e96598df0ca6e91f9
rhii0001 <rhii0001@student.monash.edu>
2024-08-15 01:49:50 +1000

    Add updateView to render the game on each tick

--- src/view.ts
+++ src/view.ts
@@ -1,4 +1,8 @@
-export { createSvgElement, hide, show };
+export { createSvgElement, hide, show, updateView };
+
+import * as Tone from "tone";
+import { Circle, Constants, Note, State, Viewport } from "./types";
+import { attr, isNotNullOrUndefined, parseCSV, playNote } from "./util";
 
 /** Rendering (side effects) */
 
@@ -38,3 +42,85 @@ const createSvgElement = (
   Object.entries(props).forEach(([k, v]) => elem.setAttribute(k, v));
   return elem;
 };
+
+const updateView =
+  (csvContents: string) =>
+  (samples: { [key: string]: Tone.Sampler }) =>
+  (onFinish: () => void) => {
+    return (state: State): void => {
+      const csv = parseCSV(csvContents);
+      const pitches = csv.map((line) => line.pitch);
+      const minPitch = Math.min(...pitches);
+      const maxPitch = Math.max(...pitches);
+      const columnSize = Math.ceil(
+        (maxPitch - minPitch) / Constants.NUMBER_OF_COLUMNS,
+      );
+
+      // Canvas elements
+      const svg = document.querySelector("#svgCanvas") as SVGGraphicsElement &
+        HTMLElement;
+      const preview = document.querySelector(
+        "#svgPreview",
+      ) as SVGGraphicsElement & HTMLElement;
+      const gameover = document.querySelector(
+        "#gameOver",
+      ) as SVGGraphicsElement & HTMLElement;
+      const container = document.querySelector("#main") as HTMLElement;
+
+      svg.setAttribute("height", `${Viewport.CANVAS_HEIGHT}`);
+      svg.setAttribute("width", `${Viewport.CANVAS_WIDTH}`);
+
+      // Text fields
+      const multiplier = document.querySelector(
+        "#multiplierText",
+      ) as HTMLElement;
+      const scoreText = document.querySelector("#scoreText") as HTMLElement;
+      const highScoreText = document.querySelector(
+        "#highScoreText",
+      ) as HTMLElement;
+
+      const updateBodyView = (rootSVG: HTMLElement) => (circle: Circle) => {
+        function createBodyView() {
+          const element = document.createElementNS(
+            rootSVG.namespaceURI,
+            "circle",
+          );
+          const color = Constants.NOTE_COLORS[circle.column - 1];
+          attr(element, {
+            id: circle.id,
+            r: Note.RADIUS,
+            cx: `${circle.x}%`,
+            style: `fill: ${color}`,
+            class: "shadow",
+          });
+          rootSVG.appendChild(element);
+          return element;
+        }
+
+        const element =
+          document.getElementById(String(circle.id)) || createBodyView();
+        attr(element, { cy: circle.y });
+      };
+
+      state.circles.forEach(updateBodyView(svg));
+
+      state.exit
+        .map((circle) => {
+          playNote(samples)(circle.note);
+          return document.getElementById(String(circle.id));
+        })
+        .filter(isNotNullOrUndefined)
+        .forEach((circle) => {
+          try {
+            svg.removeChild(circle);
+          } catch (e) {
+            console.log("Already removed: " + circle.id);
+          }
+        });
+
+      if (state.gameEnd) {
+        show(gameover);
+        onFinish();
+      }
+    };
+  };
