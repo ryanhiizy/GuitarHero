43aaaba4813ffe5a8b799d5f83f94ea09b020db4
rhii0001 <rhii0001@student.monash.edu>
2024-08-15 01:49:02 +1000

    Refactor the playNote function to always have no delay, add getColumn to get the column of the note based on its pitch, and some helper functions (not, isNotNullOrUndefined, attr)

--- src/util.ts
+++ src/util.ts
@@ -1,8 +1,15 @@
-export { formatLine, parseCSV, playNote, getColumn, createCircle };
+export {
+  formatLine,
+  parseCSV,
+  playNote,
+  getColumn,
+  not,
+  attr,
+  isNotNullOrUndefined,
+};
 
 import * as Tone from "tone";
-import { csvLine, Constants, Note } from "./types";
-import { createSvgElement } from "./view";
+import { csvLine, Constants } from "./types";
 
 /** Utility functions */
 
@@ -23,35 +30,54 @@ const parseCSV = (csvContents: string): ReadonlyArray<csvLine> => {
   return csvContents.trim().split("\n").slice(1).map(formatLine);
 };
 
-const playNote = (
-  line: csvLine,
-  samples: { [key: string]: Tone.Sampler },
-  delay: number | undefined = undefined,
-) => {
-  samples[line.instrument_name].triggerAttackRelease(
-    Tone.Frequency(line.pitch, "midi").toNote(),
-    line.end - line.start,
-    delay,
-    line.velocity / Constants.MAX_MIDI_VELOCITY,
+const playNote =
+  (samples: { [key: string]: Tone.Sampler }) => (line: csvLine) => {
+    samples[line.instrument_name].triggerAttackRelease(
+      Tone.Frequency(line.pitch, "midi").toNote(),
+      line.end - line.start,
+      undefined,
+      line.velocity / Constants.MAX_MIDI_VELOCITY,
+    );
+  };
+
+// const getColumn = (
+//   pitch: number,
+//   minPitch: number,
+//   columnSize: number,
+// ): number => Math.floor((pitch - minPitch) / columnSize) + 1;
+
+const getColumn = (pitch: number): number => {
+  const columnSize = Math.ceil(
+    Constants.MAX_MIDI_VELOCITY / Constants.NUMBER_OF_COLUMNS,
   );
+  return Math.floor(pitch / columnSize) + 1;
 };
 
-const getColumn = (
-  pitch: number,
-  minPitch: number,
-  columnSize: number,
-): number => Math.floor((pitch - minPitch) / columnSize) + 1;
+/**
+ * Composable not: invert boolean result of given function
+ * @param f a function returning boolean
+ * @param x the value that will be tested with f
+ */
+const not =
+  <T>(f: (x: T) => boolean) =>
+  (x: T) =>
+    !f(x);
 
-function createCircle(
-  column: number,
-  svg: SVGGraphicsElement & HTMLElement,
-): SVGElement {
-  const color = ["green", "red", "blue", "yellow"][column - 1];
-  return createSvgElement(svg.namespaceURI, "circle", {
-    r: `${Note.RADIUS}`,
-    cx: `${20 * column}%`,
-    cy: "0",
-    style: `fill: ${color}`,
-    class: "shadow",
-  });
+/**
+ * Type guard for use in filters
+ * @param input something that might be null or undefined
+ */
+function isNotNullOrUndefined<T extends object>(
+  input: null | undefined | T,
+): input is T {
+  return input != null;
 }
+
+/**
+ * set a number of attributes on an Element at once
+ * @param e the Element
+ * @param o a property bag
+ */
+const attr = (e: Element, o: { [p: string]: unknown }) => {
+  for (const k in o) e.setAttribute(k, String(o[k]));
+};
