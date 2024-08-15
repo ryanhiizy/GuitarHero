3af092b4a560fdf42139b25d5eebc8c38ec19f94
rhii0001 <rhii0001@student.monash.edu>
2024-08-15 20:14:23 +1000

    Add restart functionality

--- src/view.ts
+++ src/view.ts
@@ -2,7 +2,15 @@ export { createSvgElement, hide, show, updateView };
 
 import * as Tone from "tone";
 import { Circle, Constants, Note, State, Viewport } from "./types";
-import { attr, isNotNullOrUndefined, parseCSV, playNote } from "./util";
+import {
+  attr,
+  getColumn,
+  getNonOverlappingColumn,
+  isNotNullOrUndefined,
+  not,
+  parseCSV,
+  playNote,
+} from "./util";
 
 /** Rendering (side effects) */
 
@@ -46,16 +54,8 @@ const createSvgElement = (
 const updateView =
   (csvContents: string) =>
   (samples: { [key: string]: Tone.Sampler }) =>
-  (onFinish: () => void) => {
+  (onFinish: (restart: boolean) => void) => {
     return (state: State): void => {
-      const csv = parseCSV(csvContents);
-      const pitches = csv.map((line) => line.pitch);
-      const minPitch = Math.min(...pitches);
-      const maxPitch = Math.max(...pitches);
-      const columnSize = Math.ceil(
-        (maxPitch - minPitch) / Constants.NUMBER_OF_COLUMNS,
-      );
-
       // Canvas elements
       const svg = document.querySelector("#svgCanvas") as SVGGraphicsElement &
         HTMLElement;
@@ -81,10 +81,12 @@ const updateView =
 
       const updateBodyView = (rootSVG: HTMLElement) => (circle: Circle) => {
         function createBodyView() {
+          const previousCircles = Array.from({ length: 4 });
           const element = document.createElementNS(
             rootSVG.namespaceURI,
             "circle",
           );
+
           const color = Constants.NOTE_COLORS[circle.column];
           attr(element, {
             id: circle.id,
@@ -101,22 +103,40 @@ const updateView =
           document.getElementById(String(circle.id)) || createBodyView();
         attr(element, { cy: circle.y });
 
-        if (circle.y >= 350) {
-          hide(element as SVGGraphicsElement); // type assertion????
-        }
+        // if (circle.y >= 350) {
+        //   hide(element as SVGGraphicsElement); // type assertion????
+        // }
       };
 
       if (state.hitCircle) {
         const element = document.getElementById(String(state.hitCircle.id));
         if (element) {
-          playNote(samples)(state.hitCircle.note);
+          console.log("HIT");
           document.getElementById(String(state.hitCircle.id))?.remove();
+          playNote(samples)(state.hitCircle.note);
         }
       }
 
+      // console.log(state.playableCircles);
+      // const columns = Array.from({ length: 4 }) as ReadonlyArray<
+      //   Circle | undefined // type assertion again???
+      // >;
+      // const nonOverlappingColumn = getNonOverlappingColumn(columns);
+      // const overlap = (circle: Circle) => circle.y === 0;
+
+      // state.playableCircles
+      //   .filter(overlap)
+      //   .map(nonOverlappingColumn)
+      //   .forEach(({ circle, column }) =>
+      //     updateBodyView(svg)({ ...circle, column }),
+      //   );
+
+      // state.playableCircles.filter(not(overlap)).forEach(updateBodyView(svg));
+
       state.playableCircles.forEach(updateBodyView(svg));
+
       state.backgroundCircles
-        .filter((circle) => circle.duration === 1000)
+        .filter((circle) => circle.duration === 500)
         .forEach((circle) => playNote(samples)(circle.note));
 
       state.exit
@@ -134,9 +154,16 @@ const updateView =
 
       scoreText.textContent = String(state.score);
 
+      if (state.restart) {
+        const circles = document.querySelectorAll("[class=shadow]");
+        circles.forEach((circle) => circle.remove());
+        // main(csvContents, samples); // even though this doesn't run in the beginning, this line causes there to be two instances of the game running at the same time
+        onFinish(true);
+      }
+
       if (state.gameEnd) {
         show(gameover);
-        onFinish();
+        onFinish(false);
       }
     };
   };
