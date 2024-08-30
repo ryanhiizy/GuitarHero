976bd68f2762733b11808c7e84cbae66f31e4141
rhii0001 <rhii0001@student.monash.edu>
2024-08-31 05:33:56 +1000

    Documentation for view.ts

--- src/view.ts
+++ src/view.ts
@@ -1,22 +1,34 @@
 export { updateView };
 
-import { Constants, Star, State, Viewport } from "./types";
+import { Constants, State, Viewport } from "./types";
 import { attr, isNotNullOrUndefined, playRandomNote } from "./util";
 
+/**
+ * Update the SVG game view.
+ *
+ * @param s the current game model State
+ */
 const updateView = (s: State): void => {
   /**
-   * Show or hide an element based on a condition
+   * Show or hide an element based on a condition.
+   *
+   * @param id the id of the element to show or hide
+   * @param condition the condition to determine if the element should be shown or hidden
    *
    * @see https://stackblitz.com/edit/asteroids2023
    */
   const show = (id: string, condition: boolean) =>
-    ((e: HTMLElement | null) => (condition ? e?.classList.remove("hidden") : e?.classList.add("hidden")))(
+    ((e: HTMLElement | null) =>
+      condition ? e?.classList.remove("hidden") : e?.classList.add("hidden"))(
       document.getElementById(id),
     );
 
-  const svg = document.querySelector("#svgCanvas") as SVGGraphicsElement & HTMLElement;
+  const svg = document.querySelector("#svgCanvas") as SVGGraphicsElement &
+    HTMLElement;
   const container = document.querySelector("#main") as HTMLElement;
-  const starDuration = document.querySelector("#starDurationText") as HTMLElement;
+  const starDuration = document.querySelector(
+    "#starDurationText",
+  ) as HTMLElement;
   const multiplier = document.querySelector("#multiplierText") as HTMLElement;
   const score = document.querySelector("#scoreText") as HTMLElement;
   const combo = document.querySelector("#comboText") as HTMLElement;
@@ -24,19 +36,32 @@ const updateView = (s: State): void => {
   svg.setAttribute("height", `${Viewport.CANVAS_HEIGHT}`);
   svg.setAttribute("width", `${Viewport.CANVAS_WIDTH}`);
 
+  const remainingStarTime = Math.ceil(
+    (Constants.STAR_MAX_DURATION - s.starDuration) / 1000,
+  );
+
+  // Remaining star time is not 0s when the star phase is inactive so we need to check for that
+  starDuration.textContent = `${s.starDuration === 0 ? 0 : remainingStarTime}s`;
+  score.textContent = String(Math.round(s.score));
+  combo.textContent = String(s.combo);
+  multiplier.textContent = `${s.multiplier}x`;
+
   s.playableCircles.forEach((circle) => circle.updateBodyView(svg));
   s.tails.forEach((tail) => tail.updateBodyView(svg));
 
   s.clickedCircles.forEach((circle) => circle.playNote());
 
+  // Play background circles that have reached the end of their travel time
   s.bgCircles
-    .filter((circle) => circle.timePassed === Constants.NOTE_TRAVEL_TIME)
+    .filter((circle) => circle.timePassed === Constants.TRAVEL_MS)
     .forEach((circle) => circle.playNote());
 
   s.random.forEach(playRandomNote);
 
-  // Stop playing hold notes that are were released early
-  s.tails.filter((tail) => !tail.isClicked()).forEach((tail) => tail.stopNote());
+  // Stop playing hold circles that were released early
+  s.tails
+    .filter((tail) => !tail.isClicked())
+    .forEach((tail) => tail.stopNote());
 
   s.exit
     .map((circle) => document.getElementById(String(circle.id)))
@@ -50,12 +75,6 @@ const updateView = (s: State): void => {
     })
     .filter(isNotNullOrUndefined)
     .forEach((element) => svg.removeChild(element));
-
-  const starDurationTime = Math.ceil((Star.MAX_DURATION - s.starDuration) / 1000);
-  starDuration.textContent = `${s.starDuration === 0 ? 0 : starDurationTime}s`;
-  score.textContent = String(Math.round(s.score));
-  combo.textContent = String(s.combo);
-  multiplier.textContent = `${s.multiplier}x`;
 
   s.starPhase
     ? attr(container, {
