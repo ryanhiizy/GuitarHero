df42e09a75b0f088bdfd3a1eadfe278511dffc85
rhii0001 <rhii0001@student.monash.edu>
2024-08-31 10:19:00 +1000

    Documentation for circle.ts

--- src/circle.ts
+++ src/circle.ts
@@ -1,10 +1,18 @@
-export { Circle, HitCircle, Tail, BackgroundCircle, PlayableCircle, HoldCircle, StarCircle };
+export {
+  Tail,
+  Circle,
+  HitCircle,
+  HoldCircle,
+  StarCircle,
+  PlayableCircle,
+  BackgroundCircle,
+};
 
 import * as Tone from "tone";
 import { attr, calculateMultiplier, generateRandomDurationNote } from "./util";
 import {
-  Note,
   Star,
+  Note,
   State,
   ITail,
   ICircle,
@@ -24,6 +32,12 @@ abstract class Circle implements ICircle {
     public readonly sampler: Tone.Sampler,
   ) {}
 
+  /**
+   * Adds the circle to the state.
+   *
+   * @param s input State
+   * @returns updated State
+   */
   apply(s: State): State {
     return {
       ...s,
@@ -61,11 +75,19 @@ abstract class PlayableCircle<T extends IPlayableCircle<T>> extends Circle {
     this.cx = (column + 1) * Constants.COLUMN_WIDTH;
   }
 
+  /**
+   * Filters out expired circles and moves active circles on each tick.
+   *
+   * @param s input State
+   * @returns updated State
+   */
   tick(s: State): State {
     const expiredCircle = !this.isActive() ? this : null;
     const moveActiveCircle = this.isActive() ? this.moveCircle() : null;
 
-    const newPlayableCircles = moveActiveCircle ? [...s.playableCircles, moveActiveCircle] : s.playableCircles;
+    const newPlayableCircles = moveActiveCircle
+      ? [...s.playableCircles, moveActiveCircle]
+      : s.playableCircles;
     const newExit = expiredCircle ? [...s.exit, expiredCircle] : s.exit;
 
     return {
@@ -76,7 +98,7 @@ abstract class PlayableCircle<T extends IPlayableCircle<T>> extends Circle {
   }
 
   updateBodyView(rootSVG: HTMLElement) {
-    const color = Constants.NOTE_COLORS[this.column];
+    const color = Constants.COLORS[this.column];
     const createCircleSVG = () => {
       const circle = document.createElementNS(rootSVG.namespaceURI, "circle");
       attr(circle, {
@@ -89,27 +111,15 @@ abstract class PlayableCircle<T extends IPlayableCircle<T>> extends Circle {
       rootSVG.appendChild(circle);
       return circle;
     };
-    const circle = document.getElementById(String(this.id)) || createCircleSVG();
+    const circle =
+      document.getElementById(String(this.id)) || createCircleSVG();
     attr(circle, {
       cy: this.cy,
     });
   }
 
   isActive(): boolean {
-    return this.cy <= Constants.NOTE_EXPIRE_Y && !this.isClicked;
-  }
-
-  updateStateWithScore(s: State): State {
-    const newCombo = s.combo + 1;
-    const newMultiplier = calculateMultiplier(newCombo, s.multiplier);
-    const newScore = s.score + Constants.SCORE_PER_HIT * newMultiplier;
-
-    return {
-      ...s,
-      score: newScore,
-      multiplier: newMultiplier,
-      combo: newCombo,
-    };
+    return this.cy <= Constants.EXPIRED_Y && !this.isClicked;
   }
 
   abstract onClick(s: State): State;
@@ -130,8 +140,17 @@ class HitCircle extends PlayableCircle<IHitCircle> implements IHitCircle {
     super(id, note, column, sampler);
   }
 
+  // Updates score, combo, and multiplier when a hit circle is clicked.
   onClick(s: State): State {
-    return this.updateStateWithScore(s);
+    const newCombo = s.combo + 1;
+    const newMultiplier = calculateMultiplier(newCombo, s.multiplier);
+    const newScore = s.score + Constants.SCORE_PER_HIT * newMultiplier;
+    return {
+      ...s,
+      score: newScore,
+      multiplier: newMultiplier,
+      combo: newCombo,
+    };
   }
 
   moveCircle(): IHitCircle {
@@ -148,11 +167,25 @@ class HitCircle extends PlayableCircle<IHitCircle> implements IHitCircle {
   setRandomDuration(): IHitCircle {
     const randomNote = generateRandomDurationNote(this.note);
 
-    return new HitCircle(this.id, randomNote, this.column, this.sampler, this.cy, this.isClicked);
+    return new HitCircle(
+      this.id,
+      randomNote,
+      this.column,
+      this.sampler,
+      this.cy,
+      this.isClicked,
+    );
   }
 
   setClicked(isClicked: boolean): IHitCircle {
-    return new HitCircle(this.id, this.note, this.column, this.sampler, this.cy, isClicked);
+    return new HitCircle(
+      this.id,
+      this.note,
+      this.column,
+      this.sampler,
+      this.cy,
+      isClicked,
+    );
   }
 }
 
@@ -168,13 +201,20 @@ class HoldCircle extends PlayableCircle<IHoldCircle> implements IHoldCircle {
     super(id, note, column, sampler);
   }
 
+  // Override playableCircle's playNote method as hold circles are played
+  // for an indefinite duration using triggerAttack.
   playNote() {
     const note = this.note;
     const normalizedVelocity = note.velocity / Constants.MAX_MIDI_VELOCITY / 2;
 
-    this.sampler.triggerAttack(Tone.Frequency(note.pitch, "midi").toNote(), undefined, normalizedVelocity);
+    this.sampler.triggerAttack(
+      Tone.Frequency(note.pitch, "midi").toNote(),
+      undefined,
+      normalizedVelocity,
+    );
   }
 
+  // Does not update anything since hold circles are only completed when released.
   onClick(s: State): State {
     return s;
   }
@@ -193,11 +233,25 @@ class HoldCircle extends PlayableCircle<IHoldCircle> implements IHoldCircle {
   setRandomDuration(): IHoldCircle {
     const randomNote = generateRandomDurationNote(this.note);
 
-    return new HoldCircle(this.id, randomNote, this.column, this.sampler, this.cy, this.isClicked);
+    return new HoldCircle(
+      this.id,
+      randomNote,
+      this.column,
+      this.sampler,
+      this.cy,
+      this.isClicked,
+    );
   }
 
   setClicked(isClicked: boolean): IHoldCircle {
-    return new HoldCircle(this.id, this.note, this.column, this.sampler, this.cy, isClicked);
+    return new HoldCircle(
+      this.id,
+      this.note,
+      this.column,
+      this.sampler,
+      this.cy,
+      isClicked,
+    );
   }
 }
 
@@ -213,6 +267,8 @@ class StarCircle extends PlayableCircle<IStarCircle> implements IStarCircle {
     super(id, note, column, sampler);
   }
 
+  // Override playableCircle's updateBodyView method as star circles have
+  // different colors and classes.
   updateBodyView(rootSVG: HTMLElement) {
     const createCircleSVG = () => {
       const circle = document.createElementNS(rootSVG.namespaceURI, "circle");
@@ -226,17 +282,24 @@ class StarCircle extends PlayableCircle<IStarCircle> implements IStarCircle {
       rootSVG.appendChild(circle);
       return circle;
     };
-    const circle = document.getElementById(String(this.id)) || createCircleSVG();
+    const circle =
+      document.getElementById(String(this.id)) || createCircleSVG();
     attr(circle, {
       cy: this.cy,
     });
   }
 
+  // Updates score, combo, and multiplier when a star circle is clicked.
+  // Additionally, it sets the star phase to true and speeds up the game.
   onClick(s: State): State {
-    const updatedState = this.updateStateWithScore(s);
+    const newCombo = s.combo + 1;
+    const newMultiplier = calculateMultiplier(newCombo, s.multiplier);
+    const newScore = s.score + Constants.SCORE_PER_HIT * newMultiplier;
     return {
-      ...updatedState,
-      multiplier: s.starPhase ? updatedState.multiplier : updatedState.multiplier + Star.MULTIPLIER,
+      ...s,
+      score: newScore,
+      multiplier: s.starPhase ? newMultiplier : newMultiplier + Star.MULTIPLIER,
+      combo: newCombo,
       starPhase: true,
       starDuration: 0,
       delay: s.starPhase ? s.delay : s.delay + Star.DELAY,
@@ -257,11 +320,69 @@ class StarCircle extends PlayableCircle<IStarCircle> implements IStarCircle {
   setRandomDuration(): IStarCircle {
     const randomNote = generateRandomDurationNote(this.note);
 
-    return new StarCircle(this.id, randomNote, this.column, this.sampler, this.cy, this.isClicked);
+    return new StarCircle(
+      this.id,
+      randomNote,
+      this.column,
+      this.sampler,
+      this.cy,
+      this.isClicked,
+    );
   }
 
   setClicked(isClicked: boolean): IStarCircle {
-    return new StarCircle(this.id, this.note, this.column, this.sampler, this.cy, isClicked);
+    return new StarCircle(
+      this.id,
+      this.note,
+      this.column,
+      this.sampler,
+      this.cy,
+      isClicked,
+    );
+  }
+}
+
+class BackgroundCircle extends Circle implements IBackgroundCircle {
+  constructor(
+    public readonly id: number,
+    public readonly note: Note,
+    public readonly sampler: Tone.Sampler,
+    public readonly timePassed: number = 0,
+  ) {
+    super(id, note, sampler);
+  }
+
+  /**
+   * Filters out expired background circles and increments
+   * the time passed of active background circles.
+   *
+   * @param s input State
+   * @returns updated State
+   */
+  tick(s: State): State {
+    const updateBackgroundCircle = this.isActive() ? this.tickCircle() : null;
+
+    const newBackgroundCircles = updateBackgroundCircle
+      ? [...s.bgCircles, updateBackgroundCircle]
+      : s.bgCircles;
+
+    return {
+      ...s,
+      bgCircles: newBackgroundCircles,
+    };
+  }
+
+  isActive(): boolean {
+    return this.timePassed + Constants.TICK_RATE_MS <= Constants.TRAVEL_TIME;
+  }
+
+  tickCircle(): IBackgroundCircle {
+    return new BackgroundCircle(
+      this.id,
+      this.note,
+      this.sampler,
+      this.timePassed + Constants.TICK_RATE_MS,
+    );
   }
 }
 
@@ -274,6 +395,12 @@ class Tail implements ITail {
     public readonly circle: IHoldCircle,
   ) {}
 
+  /**
+   * Adds the hold circle and tail to the state.
+   *
+   * @param s input State
+   * @returns updated State
+   */
   apply(s: State): State {
     return {
       ...s,
@@ -282,12 +409,22 @@ class Tail implements ITail {
     };
   }
 
+  /**
+   * Filters out expired tails and moves active tails on each tick.
+   *
+   * @param s input State
+   * @returns updated State
+   */
   tick(s: State): State {
     const expiredTail = !this.isActive() ? this : null;
     const moveActiveTail = this.isActive() ? this.moveTail() : null;
 
-    const newTails = moveActiveTail ? [moveActiveTail, ...s.tails] : s.tails.filter((tail) => tail !== this);
-    const newExitTails = expiredTail ? [...s.exitTails, expiredTail] : s.exitTails;
+    const newTails = moveActiveTail
+      ? [moveActiveTail, ...s.tails]
+      : s.tails.filter((tail) => tail !== this);
+    const newExitTails = expiredTail
+      ? [...s.exitTails, expiredTail]
+      : s.exitTails;
 
     return {
       ...s,
@@ -297,7 +434,10 @@ class Tail implements ITail {
   }
 
   stopNote(): void {
-    this.circle.sampler.triggerRelease(Tone.Frequency(this.circle.note.pitch, "midi").toNote(), undefined);
+    this.circle.sampler.triggerRelease(
+      Tone.Frequency(this.circle.note.pitch, "midi").toNote(),
+      undefined,
+    );
   }
 
   isActive(): boolean {
@@ -306,9 +446,13 @@ class Tail implements ITail {
 
   moveTail(): Tail {
     const movedY1 = this.y1 + Constants.PIXELS_PER_TICK;
-    const newY1 = this.isClicked() ? Math.min(movedY1, Constants.TARGET_Y) : movedY1;
+    const newY1 = this.isClicked()
+      ? Math.min(movedY1, Constants.TARGET_Y)
+      : movedY1;
     const movedY2 = this.y2 + Constants.PIXELS_PER_TICK;
-    const newY2 = this.isClicked() ? Math.min(movedY2, Constants.TARGET_Y) : movedY2;
+    const newY2 = this.isClicked()
+      ? Math.min(movedY2, Constants.TARGET_Y)
+      : movedY2;
 
     return new Tail(this.id, this.x, newY1, newY2, this.circle);
   }
@@ -318,11 +462,17 @@ class Tail implements ITail {
   }
 
   setClicked(isClicked: boolean): ITail {
-    return new Tail(this.id, this.x, this.y1, this.y2, this.circle.setClicked(isClicked));
+    return new Tail(
+      this.id,
+      this.x,
+      this.y1,
+      this.y2,
+      this.circle.setClicked(isClicked),
+    );
   }
 
   updateBodyView(rootSVG: HTMLElement) {
-    const color = Constants.NOTE_COLORS[this.circle.column];
+    const color = Constants.COLORS[this.circle.column];
     const createTailSVG = () => {
       const tail = document.createElementNS(rootSVG.namespaceURI, "line");
       attr(tail, {
@@ -345,37 +495,8 @@ class Tail implements ITail {
     attr(tail, {
       y1: this.y1,
       y2: this.y2,
-      "stroke-opacity": this.y2 === Constants.TARGET_Y && this.isClicked() ? "1" : "0.25",
+      "stroke-opacity":
+        this.y2 === Constants.TARGET_Y && this.isClicked() ? "1" : "0.25",
     });
-  }
-}
-
-class BackgroundCircle extends Circle implements IBackgroundCircle {
-  constructor(
-    public readonly id: number,
-    public readonly note: Note,
-    public readonly sampler: Tone.Sampler,
-    public readonly timePassed: number = 0,
-  ) {
-    super(id, note, sampler);
-  }
-
-  tick(s: State): State {
-    const updateBackgroundCircle = this.isActive() ? this.tickCircle() : null;
-
-    const newBackgroundCircles = updateBackgroundCircle ? [...s.bgCircles, updateBackgroundCircle] : s.bgCircles;
-
-    return {
-      ...s,
-      bgCircles: newBackgroundCircles,
-    };
-  }
-
-  isActive(): boolean {
-    return this.timePassed + Constants.TICK_INTERVAL <= Constants.NOTE_TRAVEL_TIME;
-  }
-
-  tickCircle(): IBackgroundCircle {
-    return new BackgroundCircle(this.id, this.note, this.sampler, this.timePassed + Constants.TICK_INTERVAL);
   }
 }
